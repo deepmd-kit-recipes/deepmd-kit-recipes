84b9c0f8eacb73b64ba2be09c2aedbf9fefea06e
Jinzhe Zeng <jz748@amarel2.amarel.rutgers.edu>
2019-10-19 03:35:39 -0400

    update

--- lammps-dp/build.sh
+++ lammps-dp/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp -r ${PREFIX}/share/USER_DEEPMD src/
+cp -r ${PREFIX}/share/USER-DEEPMD src/
 mkdir build
 cd build
-cmake -D PKG_USER-DEEPMD=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="-DHIGH_PREC -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -ldeepmd_op -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
+cmake -D PKG_USER-DEEPMD=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="-DHIGH_PREC -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
 make -j${NUM_CPUS}
 make install
 cd ..
