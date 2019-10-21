233ca1ea14cf02bf881142a17b74d8e2aefdefe8
Restyled.io <commits@restyled.io>
2019-10-21 21:14:40 +0000

    Restyled by shellharden

--- libdeepmd/build.sh
+++ libdeepmd/build.sh
@@ -1,13 +1,13 @@
 set -e
 mkdir -p source/build
 cd source/build
-if [ ${dp_variant} == "gpu" ]; then
+if [ "$dp_variant" == "gpu" ]; then
     export CMAKE_CUDA_ARG="-DUSE_CUDA_TOOLKIT=true"
-    export CUDA_HOME=$PREFIX
+    export CUDA_HOME="$PREFIX"
 fi
-cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_CUDA_ARG} ..
-make -j${CPU_COUNT}
+cmake -DTENSORFLOW_ROOT="$PREFIX" -DCMAKE_INSTALL_PREFIX="$PREFIX" "$CMAKE_CUDA_ARG" ..
+make -j"$CPU_COUNT"
 make install
 make lammps
-mkdir -p ${PREFIX}/share
-mv USER-DEEPMD ${PREFIX}/share
+mkdir -p "$PREFIX"/share
+mv USER-DEEPMD "$PREFIX"/share
