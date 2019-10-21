d8180e57d5b0c0f3430392c25ef0283f0ee81d4d
Restyled.io <commits@restyled.io>
2019-10-21 21:14:40 +0000

    Restyled by shfmt

--- libdeepmd/build.sh
+++ libdeepmd/build.sh
@@ -2,8 +2,8 @@ set -e
 mkdir -p source/build
 cd source/build
 if [ "$dp_variant" == "gpu" ]; then
-    export CMAKE_CUDA_ARG="-DUSE_CUDA_TOOLKIT=true"
-    export CUDA_HOME="$PREFIX"
+  export CMAKE_CUDA_ARG="-DUSE_CUDA_TOOLKIT=true"
+  export CUDA_HOME="$PREFIX"
 fi
 cmake -DTENSORFLOW_ROOT="$PREFIX" -DCMAKE_INSTALL_PREFIX="$PREFIX" "$CMAKE_CUDA_ARG" ..
 make -j"$CPU_COUNT"
