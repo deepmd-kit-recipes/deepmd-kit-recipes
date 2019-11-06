85451332872017802361ae556c5c606b9af3b4b6
Restyled.io <commits@restyled.io>
2019-11-06 20:05:46 +0000

    Restyled by shellharden

--- libdeepmd/build.sh
+++ libdeepmd/build.sh
@@ -1,9 +1,9 @@
 set -e
 mkdir -p source/build
 cd source/build
-cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} ..
-make -j${CPU_COUNT}
+cmake -DTENSORFLOW_ROOT="$PREFIX" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DFLOAT_PREC="$float_prec" ..
+make -j"$CPU_COUNT"
 make install
 make lammps
-mkdir -p ${PREFIX}/share
-mv USER-DEEPMD ${PREFIX}/share
+mkdir -p "$PREFIX"/share
+mv USER-DEEPMD "$PREFIX"/share
