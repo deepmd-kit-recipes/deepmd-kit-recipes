e70658dda76d4210904abb16bc3c7b6b9859f876
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-11-06 15:04:32 -0500

    remove forcing cudatoolkit

--- libdeepmd/build.sh
+++ libdeepmd/build.sh
@@ -1,10 +1,7 @@
 set -e
 mkdir -p source/build
 cd source/build
-if [ ${dp_variant} == "gpu" ]; then
-    export CMAKE_CUDA_ARG="-DUSE_CUDA_TOOLKIT=true"
-fi
-cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_CUDA_ARG} -DFLOAT_PREC=${float_prec} ..
+cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} ..
 make -j${CPU_COUNT}
 make install
 make lammps
