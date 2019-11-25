c14ac9b1947188dc4e5c68e55f002a4cfbf33ec2
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-11-25 16:50:53 -0500

    1.1.1-2

--- lammps-dp/build.sh
+++ lammps-dp/build.sh
@@ -6,7 +6,7 @@ cd build
 if [ ${float_prec} == "high" ]; then
     export PREC_DEF="-DHIGH_PREC"
 fi
-cmake -D PKG_USER-DEEPMD=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="${PREC_DEF} -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
+cmake -D PKG_USER-DEEPMD=ON -D PKG_USER-MISC=ON -D FFT=FFTW3 -D CMAKE_INSTALL_PREFIX=${PREFIX} -D CMAKE_CXX_FLAGS="${PREC_DEF} -I${PREFIX}/include -I${PREFIX}/include/deepmd -L${PREFIX}/lib -Wl,--no-as-needed -lrt -ldeepmd_op -ldeepmd -ltensorflow_cc -ltensorflow_framework -Wl,-rpath=${PREFIX}/lib" ../cmake
 make -j${NUM_CPUS}
 make install
 cd ..
